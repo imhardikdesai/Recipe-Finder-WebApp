b4e62ea5ffc760b38dd2693c4ac2ea93ab58b4b6
Desai Hardik <87645745+imhardikdesai@users.noreply.github.com>
2023-02-05 13:01:32 +0530

    Image not loading issue solved

--- src/context/RecipeState.js
+++ src/context/RecipeState.js
@@ -1,31 +1,35 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import RecipeContext from "./RecipeContext";
-import RecipeData from "../db/RecipeData";
+// import RecipeData from "../db/RecipeData";
 
 
 const RecipeState = (props) => {
 
     const [query, setQuery] = useState('')
-    const [fetchedData, setFetchedData] = useState(RecipeData)
+    const [fetchedData, setFetchedData] = useState([])
+
+    const apiKey = '1d2df2fba8e92f32cb1529918247fe32'
+    const id = 'f0bf1ca6'
+    const url = `https://api.edamam.com/search?q=${query}&app_id=${id}&app_key=${apiKey}`
+
 
     const handleFetchRecipe = (event) => {
         if (event.key === "Enter") {
-            console.log("enter press here! ");
-            fetchRecipe()
+            fetchRecipe(url)
             setQuery('')
         }
     };
 
-    const fetchRecipe = () => {
-        const apiKey = '1d2df2fba8e92f32cb1529918247fe32'
-        const id = 'f0bf1ca6'
-
-        fetch(`https://api.edamam.com/search?q=${query}&app_id=${id}&app_key=${apiKey}`)
+    const fetchRecipe = (fetchURL) => {
+        fetch(fetchURL)
             .then(response => response.json())
             .then(data => setFetchedData(data.hits))
             .catch(err => console.error(err));
     }
 
+    useEffect(() => {
+        fetchRecipe(`https://api.edamam.com/search?q=cheery&app_id=${id}&app_key=${apiKey}`)
+    }, [])
 
     return (
         <RecipeContext.Provider value={{ query, setQuery, handleFetchRecipe, fetchedData }}>
